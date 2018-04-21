3bb982c94691303d1bf1c06df3206db5273cb97c
Alejandro Almeida <alejandro.almeida@intelequia.com>
2018-04-21 05:26:40 +0200

    Eliminados proyectos innecesarios
    El ejemplo Selfie es una solucion mucho mas completa con mas elementos interesantes

--- solution/mssql-linux/query-examples/SelectRows.sql
+++
@@ -1,9 +0,0 @@
-USE TutorialDB
-GO
-
--- Query the total count of employees
-SELECT COUNT(*) as EmployeeCount FROM dbo.Employees;
--- Query all employee information
-SELECT e.EmployeesId, e.Name, e.Location 
-FROM dbo.Employees as e
-GO
